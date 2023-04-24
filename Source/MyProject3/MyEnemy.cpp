61bc877a4f1776d050806c20a62eed9fe02f88d9
ockjisun <122524037+ockjisun@users.noreply.github.com>
2023-04-24 20:50:44 +0900

    First Update

---
+++ Source/MyProject3/MyEnemy.cpp
@@ -0,0 +1,62 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyEnemy.h"
+#include "MyAIController.h"
+#include "CreatureAnimInstance.h" 
+
+// Sets default values
+AMyEnemy::AMyEnemy()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	
+	static ConstructorHelpers::FObjectFinder<USkeletalMesh> SM(TEXT("/Script/Engine.SkeletalMesh'/Game/ParagonSevarog/Characters/Heroes/Sevarog/Meshes/Sevarog.Sevarog'"));
+
+	if (SM.Succeeded())
+	{
+		GetMesh()->SetSkeletalMesh(SM.Object);
+		GetMesh()->SetRelativeLocationAndRotation(FVector(0.f, 0.f, -88.f), FRotator(0.f, -90.f, 0.f));
+	}
+
+	AIControllerClass = AMyAIController::StaticClass();
+}
+
+// Called when the game starts or when spawned
+void AMyEnemy::BeginPlay()
+{
+	Super::BeginPlay();
+
+	AnimInstance = Cast<UCreatureAnimInstance>(GetMesh()->GetAnimInstance());
+	AnimInstance->OnMontageEnded.AddDynamic(this, &AMyEnemy::OnAttackMontageEnded);
+}
+
+// Called every frame
+void AMyEnemy::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AMyEnemy::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
+void AMyEnemy::Attack()
+{
+	
+	if (IsValid(AnimInstance))
+	{
+		AnimInstance->PlayAttackMontage();
+	}
+}
+
+void AMyEnemy::OnAttackMontageEnded(UAnimMontage* Montage, bool bInterrupted)
+{
+	IsAttacking = false;
+
+}
+
